4ca3b7fed9a78d4b9b8c87c2fed91cf01057e064
Paul Gerald Sanchez <pglsanchez@gmail.com>
2017-11-22 15:32:11 +0000

    Added tramscation

--- functionalities.py
+++ functionalities.py
@@ -6,7 +6,12 @@ def new_client():
     pass
     
 def new_transaction():
-    pass
-    
+    bank_clients = fM.clientlists()
+    print ("Here are the clients", bank_clients)
+    client_debtor = input("Name of person who will borrow money: ")
+    client_creditor = input("Name of person who will lend money: ")
+    transaction_amount = input("Transaction amount: ")
+    fM.add_banktransaction(client_debtor, client_creditor, transaction_amount)
+
 def look_credit():
     pass
